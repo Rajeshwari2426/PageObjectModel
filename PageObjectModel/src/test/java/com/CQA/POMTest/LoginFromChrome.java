4dabb14dc208d3102f5b42af6314df9dce5b0203
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-09-13 10:06:02 +0530

    [rajeshwari]adding POM

---
+++ PageObjectModel/src/test/java/com/CQA/POMTest/LoginFromChrome.java
@@ -0,0 +1,16 @@
+package com.CQA.POMTest;
+
+//import org.testng.Reporter;
+//import org.testng.annotations.BeforeMethod;
+//import org.testng.annotations.Test;
+
+public class LoginFromChrome {
+	WebDriver driver;
+	static {
+		WebDriverManager
+	}
+	
+	
+	
+
+}
